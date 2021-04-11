32e31b6af9231d12002db7098aadd102b245b058
gabriel sales <gabrielsales.fav@gmail.com>
2021-04-11 15:29:17 -0300

    Paginate

--- server.js
+++ server.js
@@ -19,15 +19,24 @@ mysqlConnection.connect(function(err){
 
 app.listen(process.env.PORT || 3000);
 
-app.get('/ListarThreads',(req,res)=>{
-    mysqlConnection.query('SELECT *,(SELECT COUNT(*) FROM threadreplies t2 WHERE t2.thread = t1.Codigo) AS respostas FROM thread t1;',(err,rows,fields)=>{
+app.get('/ListarThreads/:pagina',(req,res)=>{
+    mysqlConnection.query('SELECT count(*) as tam FROM thread;',(err,rows,fields)=>{
         if(!err){
-            res.send(rows);
+            const pages = Math.ceil(rows[0].tam/20)
+            const pagepesq = req.params.pagina;
+            mysqlConnection.query('SELECT *,(SELECT COUNT(*) FROM threadreplies t2 WHERE t2.thread = t1.Codigo) AS respostas FROM thread t1 LIMIT 20 offset ?;',(pagepesq*20)-20,(err,rows,fields)=>{
+            if(!err){
+                const ret = [{dados:rows,paginate:pages}]
+                res.send(ret);                    
+            }else{    
+                console.log(err);
+            }    
+        })
         }else{    
             console.log(err);
         }    
     })
-});
+}); 
 
 app.get('/DetalharThread/:codigo',(req,res)=>{
     const val = req.params.codigo;
@@ -62,11 +71,19 @@ app.post('/ResponderThread',(req,res)=>{
     })
 })
 
-app.get('/RespostasThread/:codigo',(req,res)=>{
-    const val = req.params.codigo;
-    mysqlConnection.query('SELECT *,date_format(dataresp,"%d/%m/%y") as dataformatada FROM threadreplies  where thread = ?;',req.params.codigo,(err,rows,fields)=>{
+app.get('/RespostasThread/:codigo/:page',(req,res)=>{
+    mysqlConnection.query('SELECT count(*) as tam FROM threadreplies  where thread = ?;',req.params.codigo,(err,rows,fields)=>{
         if(!err){
-            res.send(rows);
+            const pages = Math.ceil(rows[0].tam/20)
+            const pagepesq = req.params.page;
+            mysqlConnection.query('SELECT * FROM threadreplies WHERE thread = ? LIMIT 20 offset ?;',[req.params.codigo,(pagepesq*20)-20],(err,rows,fields)=>{
+                if(!err){
+                    const ret = [{dados:rows,paginate:pages}]
+                    res.send(ret);                    
+                }else{    
+                    console.log(err);
+                }    
+            })           
         }else{    
             console.log(err);
         }    
